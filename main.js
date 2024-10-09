8a19a45ec9752edf571081545bc170c17eded156
skillthink1 <skillthink1@gmail.com>
2024-10-09 19:26:46 +0000

    Alura-Midi 1

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function playSomidoPom(){
     document.querySelector("#sonido_tecla_pom").play();
 }
-document.querySelector(".tecla_pom")
+document.querySelector(".tecla_pom").onclick=playSomidoPom
 
 
 
