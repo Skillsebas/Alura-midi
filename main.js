a1cb59179df3ade39a52efaf97543254b3fade63
skillthink1 <skillthink1@gmail.com>
2024-10-09 02:25:17 +0000

    Alura-Midi 1

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+function playSomidoPom(){
+    document.querySelector("#sonido_tecla_pom").play();
+}
 document.querySelector(".tecla_pom")
 
-document.querySelector("#sonido_tecla_pom").play();
+
 
